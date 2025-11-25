0c8cc02247ef909ae3b29be1b8d682ff2414a6dd
Nickshay Chouhan <nickshaychouhan27@gmail.com>
2025-11-26 01:36:21 +0530

    ok

--- frontend/src/meeting/components/FloatingReactions.jsx
+++ frontend/src/meeting/components/FloatingReactions.jsx
@@ -1,7 +1,9 @@
-export default function FloatingReactions({ items }) {
+export default function FloatingReactions({ floatingReactions = [] }) {
+  if (!Array.isArray(floatingReactions)) return null;
+
   return (
     <>
-      {items.map((r) => (
+      {floatingReactions.map((r) => (
         <div
           key={r.id}
           className="absolute bottom-10 left-1/2 -translate-x-1/2 text-5xl animate-float z-50"
